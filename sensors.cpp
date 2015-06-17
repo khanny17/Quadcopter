2ceaef424a93476ea4b1f217935c0bf53d597101
Kevin <kph1958@rit.edu>
2015-06-17 13:01:09 -0400

    Comments

--- sensors.cpp
+++ sensors.cpp
@@ -5,24 +5,33 @@
 
 imu imu;
 
-Buffer<int> pitchBuffer(BUFFER_SIZE);
+//TODO we probably want to buffer each individual sensor reading,
+// instead of the pitch/roll/yaw/height value. This is in case we want to use the gyro
+// and maybe we want a larger buffer for it.
+Buffer<int> pitchBuffer(BUFFER_SIZE);  
 Buffer<int> rollBuffer(BUFFER_SIZE);
 
 SensorInterface::SensorInterface(){
-  //imu.init();
+  //imu.init(); For some reason, this was breaking it
 }
 
+/**
+ *  Updates sensor readings and returns the current PRYH
+ */
 PRYH SensorInterface::getPRYH(){
   imu.update();
   return (PRYH){ this->getPitch(), this->getRoll(), 0, 0 };
 }
 
+//Buffers the Pitch reading and returns the buffer average
+//This is to smooth the signal out
 int SensorInterface::getPitch(){
   int* acc_data = imu.getAccData();
   pitchBuffer.add(acc_data[1]);//TODO put buffer in imu?
   return pitchBuffer.average();
 }
 
+//Buffers the Roll reading and returns the buffer average
 int SensorInterface::getRoll(){
   int* acc_data = imu.getAccData();
   rollBuffer.add(acc_data[0]);
