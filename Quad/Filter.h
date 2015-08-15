2e547ec38dff14f5bb8a5fffe1b382f1944ffdeb
Kevin <kph1958@rit.edu>
2015-08-15 18:06:24 -0400

    Adding filter class

---
+++ Quad/Filter.h
@@ -0,0 +1,13 @@
+/**
+ * Abstract class for filters to combine sensor readings
+ */
+#ifndef Filter_h
+#define Filter_h
+
+class Filter
+{
+  public:
+    virtual float filter(float accReading, float gyroReading) = 0;
+}
+
+#endif
