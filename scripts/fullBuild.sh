9b51dcd89cdc2765b958e3f4995322f13181d9df
Kevin Hannigan <kph1958@rit.edu>
2016-12-01 23:08:55 -0500

    using fullbuild script and improving so it fails from linting

--- scripts/fullBuild.sh
+++ scripts/fullBuild.sh
@@ -1,8 +1,31 @@
-if [ ! -d "build" ]; then
-    mkdir build
+checkExit () {
+    if [ $? -eq 0 ]
+    then
+        echo $1
+    else
+        echo "Exiting build due to: $2" >&2
+        exit 1
+    fi
+}
+
+
+
+if [ -d "build" ]; then
+    echo "Cleaning build/"
+    rm -rf build
 fi
 
+mkdir build
 cd build
+
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-cppcheck --project=compile_commands.json --enable=all
+checkExit "" "cmake failure"
+
+cppcheck --project=compile_commands.json --enable=all --error-exitcode=1
+checkExit "" "cppcheck failure"
+
 make
+checkExit "" "make failure"
+
+make test
+checkExit "" "make test failure"
